03c2d20441f69227b4c4fa00f6ca79018ac98d4c
chengjialong <chengjialong@yy.com>
2019-09-02 17:00:48 +0800

    fix: update public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const px2rem = require('postcss-px2rem')
 
+const BASE_URL = process.env.NODE_ENV === 'production'
+  ? '/vue-simple-noticebar-demo/'
+  : '/'
+
 module.exports = {
+  publicPath: BASE_URL,
   css: {
     loaderOptions: {
         postcss: {
